064c2abc041ab646fcc9a3bdeb715298edee458f
AlGrimd <grimeaud.alex@gmail.com>
2025-12-16 14:06:47 +0100

    Update connection_db.php

--- connection_db.php
+++ connection_db.php
@@ -1,21 +1,12 @@
 <?php
-// Connexion à la base MySQL (sera utilisée plus tard)
-
 $host = "localhost";
-$db   = "smartfarm";   // ta future base
 $user = "root";
-$pass = "";            // vide sur XAMPP
+$password = "";
+$dbname = "smartfarm";
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (PDOException $e) {
-    // Pour l’instant on affiche rien (silencieux)
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Erreur connexion DB : " . $conn->connect_error);
 }
+?>
