cf3a1a24e66dd24c460a53301bd537d5efdc8060
Alex <alex.grimaud@isen-ouest.yncrea.fr>
2026-03-06 19:02:09 +0100

    Connexion site web avec base de données + script dashboard

--- api_data.php
+++ api_data.php
@@ -1,50 +1,37 @@
 <?php
 
-if (file_exists("connexion_db.php")) {
-    require("connexion_db.php");
-}
+require("connexion_db.php");
 
 $farm = $_GET['farm'] ?? 1;
 
 try {
 
-    // =========================
-    // DONNÉES DE LA FERME
-    // =========================
+    // données de la ferme
     $stmt = $pdo->prepare("
-        SELECT *
-        FROM sensor_data
+        SELECT * 
+        FROM sensor_data 
         WHERE farm_id = ?
     ");
-
     $stmt->execute([$farm]);
     $farmData = $stmt->fetch(PDO::FETCH_ASSOC);
 
-
-    // =========================
-    // MOYENNES GLOBALES
-    // =========================
+    // moyennes globales ARRONDIES
     $stmt2 = $pdo->prepare("
-    SELECT
-        ROUND(AVG(air_temp),1) as temp_global,
-        ROUND(AVG(air_humidity),1) as humi_global,
-        ROUND(AVG(water_level),1) as water_global,
-        ROUND(AVG(photoresistor),0) as light_global,
-        ROUND(AVG(consommation),1) as energy_global
-    FROM sensor_data
+        SELECT
+            ROUND(AVG(air_temp),1) as temp_global,
+            ROUND(AVG(air_humidity),0) as humi_global,
+            ROUND(AVG(water_level),0) as water_global,
+            ROUND(AVG(photoresistor),0) as light_global,
+            ROUND(AVG(consommation),1) as energy_global
+        FROM sensor_data
     ");
 
-    $stmtGlobal->execute();
-    $globalData = $stmtGlobal->fetch(PDO::FETCH_ASSOC);
+    $stmt2->execute();
+    $globalData = $stmt2->fetch(PDO::FETCH_ASSOC);
 
-
-    // =========================
-    // FUSION DES DONNÉES
-    // =========================
     $data = array_merge($farmData, $globalData);
 
     header('Content-Type: application/json');
-
     echo json_encode($data);
 
 } catch(PDOException $e){
@@ -54,5 +41,3 @@ try {
     ]);
 
 }
-
-?>
