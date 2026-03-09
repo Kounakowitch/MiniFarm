cb0a6998156433cdb4c98012f20de2bd37611871
Zinhmar <136445999+Zinhmar@users.noreply.github.com>
2026-03-09 14:48:34 +0100

    Add files via upload

---
+++ main -Ferme4/main/connect.h
@@ -0,0 +1,33 @@
+#ifndef CONNECT_H
+#define CONNECT_H
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+
+extern WiFiClient espClient;
+extern PubSubClient client;
+extern LiquidCrystal_I2C lcd;
+
+extern const char* ssid;
+extern const char* password;
+extern const char* mqtt_server;
+extern const int mqtt_port;
+
+extern String FERME_ID;
+
+// Variables de comptage
+extern int nbMax;
+extern int countF4;
+extern int lastCountF2;
+
+// Prototypes
+void setup_wifi();
+void reconnect();
+void setup_LCD();
+void callback(char* topic, byte* payload, unsigned int length);
+void publishCountF4();
+void displayCount();
+void displayFinished();
+
+#endif
