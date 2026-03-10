7d9b9703180fe21ec645c61a175bcf5b920b4479
Alex <alex.grimaud@isen-ouest.yncrea.fr>
2026-03-10 10:03:11 +0100

    maj des barres de progression

--- ferme1.php
+++ ferme1.php
@@ -52,14 +52,11 @@
 
             <div class="control-card control-fog">
             <h4><i class="fas fa-smog"></i> Niveau de Brouillard</h4>
-            <p>Intensité : <span id="steam_sensor">--</span></p>
             
             <div class="fog-bar-container" style="width: 100%; background-color: #eee; border-radius: 5px; height: 15px; margin: 10px 0;">
                 <div id="fog-bar" style="height: 100%; width: 0%; background-color: #3498db; border-radius: 5px; transition: width 0.5s;"></div>
             </div>
-            <p class="alert-message" id="fog-alert-msg" style="margin-top:10px;">
-                <i class="fas fa-info-circle"></i> <span id="fog-advice">Calcul des données...</span>
-            </p>
+            
 
     
 
